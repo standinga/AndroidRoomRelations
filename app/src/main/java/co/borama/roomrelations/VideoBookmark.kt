1142d865d7a5b42b3713e314db71cede93f80cad
michal bojanowicz <boramaapps@gmail.com>
2019-03-30 14:15:48 +0100

    updated VideoBookmark entity

--- app/src/main/java/co/borama/roomrelations/VideoBookmark.kt
+++ app/src/main/java/co/borama/roomrelations/VideoBookmark.kt
@@ -1,6 +1,8 @@
 package co.borama.roomrelations
 
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
+import android.arch.persistence.room.ForeignKey.CASCADE
 import android.arch.persistence.room.PrimaryKey
 
 @Entity(foreignKeys = [(ForeignKey(
